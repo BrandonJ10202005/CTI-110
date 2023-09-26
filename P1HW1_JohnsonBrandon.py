db86c19fbb5d208923f89c985f06b92b290a4835
BrandonJ10202005 <146122482+BrandonJ10202005@users.noreply.github.com>
2023-09-26 14:37:49 -0400

    Add files via upload

---
+++ P1HW1_JohnsonBrandon.py
@@ -0,0 +1,16 @@
+#Brandon Johnson
+#9/26/2023
+#Inputs, Outputs, and Mathematical Expressions
+
+num1 = int(input("Enter A Number: "))
+
+print("You entered:", num1)
+print(num1, "Squared is:", num1**2)
+print("And", num1, "Cubed is:", num1**3)
+
+num2 = int(input("Enter Another Number: "))
+
+print(num1, "+", num2, "=", num1+num2)
+print(num1, "*", num2, "=", num1*num2)
+
+
